3695db4b2a9b850424bcd4dba7a5965e68797332
daustindev <daustindev@gmail.com>
2021-05-28 09:33:39 -0500

    Generated Models

---
+++ RecipeWebApp/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class AppUser
+{
+	public AppUser()
+	{
+		Public string ID { get; set; }
+
+		Public string Password { get; set; }
+
+		Public string Name { get; set; }
+	}
+}
